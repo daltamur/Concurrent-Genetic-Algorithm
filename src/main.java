ca56048819290f59a870ce51d70ab4f2e9019152
Dominic Altamura <daltamur@oswego.edu>
2021-09-21 21:01:00 -0400

    separated into separate functions now

--- src/main.java
+++ src/main.java
@@ -38,10 +38,9 @@ public class main {
         //maximum amount of holes allowed, counts down to zero as holes are placed
 
         //System.out.println(fitnessMeasurment(floorSpace));
-        List<Future<member>> allFloors=getFirstGen();
+        List<member> allFloors=getFirstGen();
         for(int i=0;i<300;i++){
-            Future<member> future=allFloors.get(i);
-            member member=future.get();
+            member member=allFloors.get(i);
             Integer[][] thisFloor=member.getRoom();
             if(i==299){
                 System.out.println("Final floor");
@@ -56,153 +55,44 @@ public class main {
             System.out.println(member.getFitness());
             System.out.println("++++++++++++");
         }
-        List<member>selectionList=new ArrayList<>();
-        List<member>selectionListSyn=Collections.synchronizedList(selectionList);
-        ExecutorService selectionservice=Executors.newFixedThreadPool(coresForProcessing-1);
-        final AtomicInteger selectionListIndex=new AtomicInteger(0);
-        List<Future<member>> finalAllFloors = allFloors;
-        Callable<member> selection=()->{
-            int curIndex;
-            curIndex=selectionListIndex.get();
-            selectionListIndex.getAndAdd(1);
-            Future<member> curMemberFuture= finalAllFloors.get(curIndex);
-            member curMember=curMemberFuture.get();
-            int numberOfEntries=(int)Math.floor(100*curMember.getFitness());
-            System.out.println("current index is: "+curIndex+" Fitness is: "+curMember.getFitness()+" Number of entries: "+numberOfEntries);
-            for(int i=0;i<numberOfEntries;i++){
-                selectionListSyn.add(curMember);
-            }
-            return curMember;
-        };
-        List<Callable<member>>roulette=new ArrayList<>();
-        for(int i=0;i<300;i++){
-            roulette.add(selection);
-        }
         //simple way to just make sure everything gets finished before anything else happens.
-        List<Future<member>>allFutures=service.invokeAll(roulette);
-        System.out.println(selectionListSyn.size());
-        member selectedMember=selectionListSyn.get(ThreadLocalRandom.current().nextInt(0, selectionListSyn.size()));
-        member selectedMemberMate=selectionListSyn.get(ThreadLocalRandom.current().nextInt(0, selectionListSyn.size()));
-        do{
-            selectedMemberMate=selectionListSyn.get(ThreadLocalRandom.current().nextInt(0, selectionListSyn.size()));
-        }while (selectedMember.getFitness()==selectedMemberMate.getFitness());
+        List<member>parents=getParents(allFloors);
+        member selectedMember=parents.get(0);
+        member selectedMemberMate=parents.get(1);
         System.out.println("Fitness is: "+selectedMember.getFitness());
         System.out.println("Fitness of mate is "+selectedMemberMate.getFitness());
 
-        member finalSelectedMemberMate = selectedMemberMate;
         List<member>childrenList=new ArrayList<>();
-        List<member>childrenListSyn=Collections.synchronizedList(childrenList);
-        Callable <member> createChildren=()->{
-            int crossOverPoint=ThreadLocalRandom.current().nextInt(0,rowAmount);
-          //left side is initial parent, right side is the mate
-            member thisMember=new member();
-            boolean onFinalRow= spaces <= 10;
-            Integer[][] childFloorSpace =new Integer[rowAmount][];
-            for (int i=0;i<rowAmount;i++){
-                if (!onFinalRow){
-                    childFloorSpace[i]=new Integer[10];
-                    for(int x=0;x<10;x++) {
-                        //random number between 0 (reps a hole) and the amount of flavors
-                        if(x<=crossOverPoint){
-                            childFloorSpace[i][x]=selectedMember.getRoom()[i][x];
-                            thisMember.addLocation(x, i, childFloorSpace[i][x]);
-                        }else{
-                            childFloorSpace[i][x]= finalSelectedMemberMate.getRoom()[i][x];
-                            thisMember.addLocation(x, i, childFloorSpace[i][x]);
-                        }
-                        thisMember.addLocation(x, i, childFloorSpace[i][x]);
-                    }
-                    if(i+1==rowAmount-1){
-                        onFinalRow =true;
-                    }
-                }else{
-                    if(remainderRow!=0) {
-                        childFloorSpace[i] = new Integer[remainderRow];
-                        for (int x = 0; x < remainderRow; x++) {
-                            //random number between 0 (reps a hole) and the amount of flavors
-                            if(x<=crossOverPoint){
-                                childFloorSpace[i][x]=selectedMember.getRoom()[i][x];
-                                thisMember.addLocation(x, i, childFloorSpace[i][x]);
-                            }else{
-                                childFloorSpace[i][x]= finalSelectedMemberMate.getRoom()[i][x];
-                                thisMember.addLocation(x, i, childFloorSpace[i][x]);
-                            }
-                            //will only make the hole if the random boolean is true and we can afford to add more holes
-                        }
-                    }else{
-                        childFloorSpace[i] = new Integer[10];
-                        for (int x = 0; x < 10; x++) {
-                            //random number between 0 (reps a hole) and the amount of flavors
-                            if(x<=crossOverPoint){
-                                childFloorSpace[i][x]=selectedMember.getRoom()[i][x];
-                                thisMember.addLocation(x, i, childFloorSpace[i][x]);
-                            }else{
-                                childFloorSpace[i][x]= finalSelectedMemberMate.getRoom()[i][x];
-                                thisMember.addLocation(x, i, childFloorSpace[i][x]);
-                            }
-                        }
-                    }
+        List<member>childrenListSyn=childrenRun(selectedMemberMate,selectedMember);
+        int i=0;
+        while(i<300){
+            parents=getParents(childrenListSyn);
+            selectedMember=parents.get(0);
+            selectedMemberMate=parents.get(1);
+            System.out.println("Fitness is: "+selectedMember.getFitness());
+            System.out.println("Fitness of mate is "+selectedMemberMate.getFitness());
+            childrenListSyn=childrenRun(selectedMemberMate,selectedMember);
+            i++;
+            for(int x=0;x<300;x++){
+                member member=childrenListSyn.get(x);
+                Integer[][] thisFloor=member.getRoom();
+                if(x==299){
+                    System.out.println("Final floor");
                 }
-            }
-            thisMember.addRoom(childFloorSpace);
-
-            ArrayList<Double> averageLengths=new ArrayList<>();
-            for (Integer key: thisMember.getHashmap().keySet()) {
-                ArrayList<Integer[]> coords=thisMember.getHashmap().get(key);
-                //determine the fitness based on the highest proximity of a machine compared to the other machines
-                double curFitness=0;
-                double currentBestAverage;
-                if(coords.size()>1&&key!=0){
-                    double thisAverage=0;
-                    for(int i=0;i<coords.size();i++){
-                        for (Integer[] coord : coords) {
-                            //maybe change this so we get the best average distance of key and average those best averages together with the affinity metric...we'll see how it goes first
-                            double distance = Math.sqrt((Math.pow((coords.get(i)[0] - coord[0]), 2)) + (Math.pow((coords.get(i)[1] - coord[1]), 2)));
-                            thisAverage += distance;
-                        }
+                for(int y=0;y<rowAmount;y++){
+                    for(int z=0;z<thisFloor[y].length ;z++){
+                        System.out.print(thisFloor[y][z]+"|");
                     }
-                    thisAverage=(thisAverage/(coords.size()*(coords.size()-1)))/key;
-                    averageLengths.add(thisAverage);
+                    System.out.println();
+                    System.out.println("________________________");
                 }
+                System.out.println(member.getFitness());
+                System.out.println("++++++++++++");
             }
-            double averageBetweenTwoSameValues=0.0;
-            for (Double averageLength : averageLengths) {
-                averageBetweenTwoSameValues += averageLength;
-            }
-            averageBetweenTwoSameValues=averageBetweenTwoSameValues/averageLengths.size();
-            thisMember.changeFitness(1/averageBetweenTwoSameValues);
-            childrenListSyn.add(thisMember);
-         return thisMember;
-        };
-
-        tasks=new ArrayList<>();
-        List<Future<member>> allFloorsholder;
-        for(int i=0;i<300;i++){
-            tasks.add(createChildren);
         }
-
-        allFloorsholder=service.invokeAll(tasks);
-        System.out.println("CHILDREN: ");
-        for(int i=0;i<300;i++){
-            member member=childrenListSyn.get(i);
-            Integer[][] thisFloor=member.getRoom();
-            if(i==299){
-                System.out.println("Final floor");
-            }
-            for(int y=0;y<rowAmount;y++){
-                for(int x=0;x<thisFloor[y].length ;x++){
-                    System.out.print(thisFloor[y][x]+"|");
-                }
-                System.out.println();
-                System.out.println("________________________");
-            }
-            System.out.println(member.getFitness());
-            System.out.println("++++++++++++");
-        }
-
     }
 
-    public static List<Future<member>> getFirstGen() throws InterruptedException {
+    public static List<member> getFirstGen() throws InterruptedException, ExecutionException {
         Callable<member> roomCreation= () -> {
             member thisMember=new member();
             boolean onFinalRow= spaces <= 10;
@@ -291,15 +181,18 @@ public class main {
         for(int i=0;i<300;i++){
             tasks.add(roomCreation);
         }
-
         allFloors=service.invokeAll(tasks);
-        return allFloors;
+        List<member>finalVal=new ArrayList<>();
+        for (Future<member> allFloor : allFloors) {
+            finalVal.add(allFloor.get());
+        }
+        return finalVal;
 
 
     }
 
 
-    public static List<Member> childrenRun(member finalSelectedMemberMate, member selectedMember){
+    public static List<member> childrenRun(member finalSelectedMemberMate, member selectedMember) throws InterruptedException, ExecutionException {
         List<member>childrenList=new ArrayList<>();
         List<member>childrenListSyn=Collections.synchronizedList(childrenList);
         Callable <member> createChildren=()->{
@@ -384,12 +277,52 @@ public class main {
             childrenListSyn.add(thisMember);
             return thisMember;
         };
+        List<Callable<member>>tasks=new ArrayList<>();
+        List<Future<member>> allFloors;
+        for(int i=0;i<300;i++){
+            tasks.add(createChildren);
+        }
+        allFloors=service.invokeAll(tasks);
+        List<member>finalVal=new ArrayList<>();
+        for (Future<member> allFloor : allFloors) {
+            finalVal.add(allFloor.get());
+        }
+        return childrenListSyn;
 
-        return null;
     }
 
-    public static List<member> getParents(){
+    public static List<member> getParents(List<member> lastGen) throws InterruptedException {
+        List<member>selectionList=new ArrayList<>();
+        List<member>selectionListSyn=Collections.synchronizedList(selectionList);
         List<member> parents=new ArrayList<>();
+        final AtomicInteger selectionListIndex=new AtomicInteger(0);
+        Callable<member> selection=()->{
+            int curIndex;
+            curIndex=selectionListIndex.get();
+            selectionListIndex.getAndAdd(1);
+            member curMember=lastGen.get(curIndex);
+            int numberOfEntries=(int)Math.floor(100*curMember.getFitness());
+            System.out.println("current index is: "+curIndex+" Fitness is: "+curMember.getFitness()+" Number of entries: "+numberOfEntries);
+            for(int i=0;i<numberOfEntries;i++){
+                selectionListSyn.add(curMember);
+            }
+            return curMember;
+        };
+        List<Callable<member>>roulette=new ArrayList<>();
+        for(int i=0;i<300;i++){
+            roulette.add(selection);
+        }
+        List<Future<member>>allFutures=service.invokeAll(roulette);
+        member selectedMember=selectionListSyn.get(ThreadLocalRandom.current().nextInt(0, selectionListSyn.size()));
+        member selectedMemberMate=selectionListSyn.get(ThreadLocalRandom.current().nextInt(0, selectionListSyn.size()));
+        do{
+            selectedMemberMate=selectionListSyn.get(ThreadLocalRandom.current().nextInt(0, selectionListSyn.size()));
+        }while (selectedMember.getFitness()==selectedMemberMate.getFitness());
+        //simple way to just make sure everything gets finished before anything else happens.
+        System.out.println(selectionListSyn.size());
+        parents.add(selectedMember);
+        parents.add(selectedMemberMate);
+        return parents;
     }
 
 }
