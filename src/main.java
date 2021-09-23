4dac186df37750a22e335d3efe34fa3cf0045623
Dominic Altamura <daltamur@oswego.edu>
2021-09-23 14:47:38 -0400

    Just a commit to go back if new fitness function is garbo

--- src/main.java
+++ src/main.java
@@ -15,7 +15,7 @@ public class main {
     public static int machines;
     //save 1 thread for UI
     public static int coresForProcessing=cores-1;
-    public static ExecutorService service= Executors.newWorkStealingPool(coresForProcessing);
+    public static ExecutorService service= Executors.newWorkStealingPool();
     //the final row will hold the last of the spaces if need be, this number indicates how many spaces are in the row
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         int cores = Runtime.getRuntime().availableProcessors();
@@ -55,6 +55,8 @@ public class main {
             System.out.println(member.getFitness());
             System.out.println("++++++++++++");
         }
+        //comment this out
+        //allFloors=getFirstGen();
         //simple way to just make sure everything gets finished before anything else happens.
         List<member>parents=getParents(allFloors);
         member selectedMember=parents.get(0);
@@ -65,13 +67,14 @@ public class main {
         List<member>childrenList=new ArrayList<>();
         List<member>childrenListSyn=childrenRun(selectedMemberMate,selectedMember);
         int i=0;
-        while(i<300){
+        while(i<1000){
             parents=getParents(childrenListSyn);
             selectedMember=parents.get(0);
             selectedMemberMate=parents.get(1);
             System.out.println("Fitness is: "+selectedMember.getFitness());
             System.out.println("Fitness of mate is "+selectedMemberMate.getFitness());
             childrenListSyn=childrenRun(selectedMemberMate,selectedMember);
+            //childrenListSyn=getFirstGen();
             i++;
             for(int x=0;x<300;x++){
                 member member=childrenListSyn.get(x);
@@ -93,7 +96,10 @@ public class main {
     }
 
     public static List<member> getFirstGen() throws InterruptedException, ExecutionException {
-        Callable<member> roomCreation= () -> {
+        CountDownLatch latch = new CountDownLatch(300);
+        List<member>finalVal=new ArrayList<>();
+        List<member>synList=Collections.synchronizedList(finalVal);
+        Runnable roomCreation= () -> {
             member thisMember=new member();
             boolean onFinalRow= spaces <= 10;
             int maxHolesLeft=spaces-machines;
@@ -160,11 +166,11 @@ public class main {
                     for(int i=0;i<coords.size();i++){
                         for (Integer[] coord : coords) {
                             //maybe change this so we get the best average distance of key and average those best averages together with the affinity metric...we'll see how it goes first
-                            double distance = Math.sqrt((Math.pow((coords.get(i)[0] - coord[0]), 2)) + (Math.pow((coords.get(i)[1] - coord[1]), 2)));
+                            double distance = Math.sqrt((Math.pow((coords.get(i)[0] - coord[0]), 2)) + (Math.pow((coords.get(i)[1] - coord[1]), 2)))/key;
                             thisAverage += distance;
                         }
                     }
-                    thisAverage=(thisAverage/(coords.size()*(coords.size()-1)))/key;
+                    thisAverage=(thisAverage/(coords.size()*(coords.size()-1)));
                     averageLengths.add(thisAverage);
                 }
             }
@@ -174,28 +180,34 @@ public class main {
             }
             averageBetweenTwoSameValues=averageBetweenTwoSameValues/averageLengths.size();
             thisMember.changeFitness(1/averageBetweenTwoSameValues);
-            return thisMember;
+            synList.add(thisMember);
+            try {
+                Thread.currentThread().join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            latch.countDown();
         };
-        List<Callable<member>>tasks=new ArrayList<>();
         List<Future<member>> allFloors;
         for(int i=0;i<300;i++){
-            tasks.add(roomCreation);
+            service.execute(roomCreation);
         }
-        allFloors=service.invokeAll(tasks);
-        List<member>finalVal=new ArrayList<>();
-        for (Future<member> allFloor : allFloors) {
-            finalVal.add(allFloor.get());
+
+        while (latch.getCount()>0) {
+            latch.await();
         }
-        return finalVal;
+        return synList;
 
 
     }
 
 
     public static List<member> childrenRun(member finalSelectedMemberMate, member selectedMember) throws InterruptedException, ExecutionException {
+        //service=Executors.newWorkStealingPool();
+        CountDownLatch latch = new CountDownLatch(300);
         List<member>childrenList=new ArrayList<>();
         List<member>childrenListSyn=Collections.synchronizedList(childrenList);
-        Callable <member> createChildren=()->{
+        Runnable createChildren=()->{
             int crossOverPoint=ThreadLocalRandom.current().nextInt(0,rowAmount);
             //left side is initial parent, right side is the mate
             member thisMember=new member();
@@ -206,14 +218,20 @@ public class main {
                     childFloorSpace[i]=new Integer[10];
                     for(int x=0;x<10;x++) {
                         //random number between 0 (reps a hole) and the amount of flavors
-                        if(x<=crossOverPoint){
+                        double mutationProb=ThreadLocalRandom.current().nextDouble(0,1);
+                        //random number between 0 (reps a hole) and the amount of flavors
+                        if(mutationProb>.1){
+                            childFloorSpace[i][x]=ThreadLocalRandom.current().nextInt(1, flavors+1);
+                            thisMember.addLocation(x, i, childFloorSpace[i][x]);
+                        }else if(x<=crossOverPoint){
+                            //childFloorSpace[i][x] = ThreadLocalRandom.current().nextInt(1, flavors+1);
                             childFloorSpace[i][x]=selectedMember.getRoom()[i][x];
                             thisMember.addLocation(x, i, childFloorSpace[i][x]);
                         }else{
+                            //childFloorSpace[i][x] = ThreadLocalRandom.current().nextInt(1, flavors+1);
                             childFloorSpace[i][x]= finalSelectedMemberMate.getRoom()[i][x];
                             thisMember.addLocation(x, i, childFloorSpace[i][x]);
                         }
-                        thisMember.addLocation(x, i, childFloorSpace[i][x]);
                     }
                     if(i+1==rowAmount-1){
                         onFinalRow =true;
@@ -222,11 +240,17 @@ public class main {
                     if(remainderRow!=0) {
                         childFloorSpace[i] = new Integer[remainderRow];
                         for (int x = 0; x < remainderRow; x++) {
+                            double mutationProb=ThreadLocalRandom.current().nextDouble(0,1);
                             //random number between 0 (reps a hole) and the amount of flavors
-                            if(x<=crossOverPoint){
+                            if(mutationProb>.1){
+                                childFloorSpace[i][x]=ThreadLocalRandom.current().nextInt(1, flavors+1);
+                                thisMember.addLocation(x, i, childFloorSpace[i][x]);
+                            }else if(x<=crossOverPoint){
+                                //childFloorSpace[i][x] = ThreadLocalRandom.current().nextInt(1, flavors+1);
                                 childFloorSpace[i][x]=selectedMember.getRoom()[i][x];
                                 thisMember.addLocation(x, i, childFloorSpace[i][x]);
                             }else{
+                                //childFloorSpace[i][x] = ThreadLocalRandom.current().nextInt(1, flavors+1);
                                 childFloorSpace[i][x]= finalSelectedMemberMate.getRoom()[i][x];
                                 thisMember.addLocation(x, i, childFloorSpace[i][x]);
                             }
@@ -236,10 +260,17 @@ public class main {
                         childFloorSpace[i] = new Integer[10];
                         for (int x = 0; x < 10; x++) {
                             //random number between 0 (reps a hole) and the amount of flavors
-                            if(x<=crossOverPoint){
+                            double mutationProb=ThreadLocalRandom.current().nextDouble(0,1);
+                            //random number between 0 (reps a hole) and the amount of flavors
+                            if(mutationProb>.1){
+                                childFloorSpace[i][x]=ThreadLocalRandom.current().nextInt(1, flavors+1);
+                                thisMember.addLocation(x, i, childFloorSpace[i][x]);
+                            }else if(x<=crossOverPoint){
+                                //childFloorSpace[i][x] = ThreadLocalRandom.current().nextInt(1, flavors+1);
                                 childFloorSpace[i][x]=selectedMember.getRoom()[i][x];
                                 thisMember.addLocation(x, i, childFloorSpace[i][x]);
                             }else{
+                                //childFloorSpace[i][x] = ThreadLocalRandom.current().nextInt(1, flavors+1);
                                 childFloorSpace[i][x]= finalSelectedMemberMate.getRoom()[i][x];
                                 thisMember.addLocation(x, i, childFloorSpace[i][x]);
                             }
@@ -247,6 +278,30 @@ public class main {
                     }
                 }
             }
+            //now make sure there is the right number of holes
+            //if there are too many holes, fill up as many as needed
+            int numberOfHoles=0;
+            if(thisMember.getHashmap().containsKey(0)) {
+                numberOfHoles = thisMember.getHashmap().get(0).size();
+            }
+            if(numberOfHoles>(spaces-machines)){
+                ArrayList<Integer[]>holesCoordinates=thisMember.getHashmap().get(0);
+                int excessHoles=holesCoordinates.size()-(spaces-machines);
+                while(excessHoles>0){
+                    //randomly fill a chosen index value with an int until no more extra holes
+                    int randomIndex=ThreadLocalRandom.current().nextInt(0,holesCoordinates.size());
+                    Integer[] removedCoords =holesCoordinates.get(randomIndex);
+                    int xVal=removedCoords[0];
+                    int yVal=removedCoords[1];
+                    thisMember.removeValueFromHashmap(xVal,yVal,0);
+                    int newVal=ThreadLocalRandom.current().nextInt(1, flavors+1);
+                    childFloorSpace[xVal][yVal]=newVal;
+                    thisMember.addLocation(xVal,yVal,newVal);
+                    excessHoles--;
+                }
+            }
+
+
             thisMember.addRoom(childFloorSpace);
 
             ArrayList<Double> averageLengths=new ArrayList<>();
@@ -260,12 +315,12 @@ public class main {
                     for(int i=0;i<coords.size();i++){
                         for (Integer[] coord : coords) {
                             //maybe change this so we get the best average distance of key and average those best averages together with the affinity metric...we'll see how it goes first
-                            double distance = Math.sqrt((Math.pow((coords.get(i)[0] - coord[0]), 2)) + (Math.pow((coords.get(i)[1] - coord[1]), 2)));
+                            double distance = Math.sqrt((Math.pow((coords.get(i)[0] - coord[0]), 2)) + (Math.pow((coords.get(i)[1] - coord[1]), 2)))/key;
                             thisAverage += distance;
                         }
                     }
-                    thisAverage=(thisAverage/(coords.size()*(coords.size()-1)))/key;
-                    averageLengths.add(thisAverage);
+                    thisAverage=(thisAverage/(coords.size()*(coords.size()-1)));
+                    averageLengths.add(1/thisAverage);
                 }
             }
             double averageBetweenTwoSameValues=0.0;
@@ -273,30 +328,34 @@ public class main {
                 averageBetweenTwoSameValues += averageLength;
             }
             averageBetweenTwoSameValues=averageBetweenTwoSameValues/averageLengths.size();
-            thisMember.changeFitness(1/averageBetweenTwoSameValues);
+            thisMember.changeFitness(averageBetweenTwoSameValues);
             childrenListSyn.add(thisMember);
-            return thisMember;
+            latch.countDown();
         };
         List<Callable<member>>tasks=new ArrayList<>();
         List<Future<member>> allFloors;
         for(int i=0;i<300;i++){
-            tasks.add(createChildren);
+            //tasks.add(createChildren);
+            service.execute(createChildren);
         }
-        allFloors=service.invokeAll(tasks);
-        List<member>finalVal=new ArrayList<>();
-        for (Future<member> allFloor : allFloors) {
-            finalVal.add(allFloor.get());
+        while (latch.getCount()>0) {
+            latch.await();
         }
+        //service.shutdown();
+        //for (Future<member> allFloor : childrenListSyn) {
+        //    finalVal.add(allFloor.get());
+        //}
         return childrenListSyn;
 
     }
 
-    public static List<member> getParents(List<member> lastGen) throws InterruptedException {
+    public static List<member> getParents(List<member> lastGen) throws InterruptedException, ExecutionException {
         List<member>selectionList=new ArrayList<>();
         List<member>selectionListSyn=Collections.synchronizedList(selectionList);
         List<member> parents=new ArrayList<>();
         final AtomicInteger selectionListIndex=new AtomicInteger(0);
-        Callable<member> selection=()->{
+        Callable<List<member>> selection=()->{
+            List<member> thisMemberCount=new ArrayList<>();
             int curIndex;
             curIndex=selectionListIndex.get();
             selectionListIndex.getAndAdd(1);
@@ -304,15 +363,20 @@ public class main {
             int numberOfEntries=(int)Math.floor(100*curMember.getFitness());
             System.out.println("current index is: "+curIndex+" Fitness is: "+curMember.getFitness()+" Number of entries: "+numberOfEntries);
             for(int i=0;i<numberOfEntries;i++){
-                selectionListSyn.add(curMember);
+                thisMemberCount.add(curMember);
+                //selectionListSyn.add(curMember);
             }
-            return curMember;
+            return thisMemberCount;
         };
-        List<Callable<member>>roulette=new ArrayList<>();
+        List<Callable<List<member>>>roulette=new ArrayList<>();
         for(int i=0;i<300;i++){
             roulette.add(selection);
         }
-        List<Future<member>>allFutures=service.invokeAll(roulette);
+        List<Future<List<member>>>allFutures=service.invokeAll(roulette);
+        for (Future<List<member>> allFloor : allFutures) {
+            selectionListSyn.addAll(allFloor.get());
+        }
+        //service.shutdown();
         member selectedMember=selectionListSyn.get(ThreadLocalRandom.current().nextInt(0, selectionListSyn.size()));
         member selectedMemberMate=selectionListSyn.get(ThreadLocalRandom.current().nextInt(0, selectionListSyn.size()));
         do{
